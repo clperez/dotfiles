b34f38addd25d70e8f8ad7b13755fa92aaf93bb5
Carlos L. Perez <carlillos34@gmail.com>
2022-02-21 12:10:37 +0000

    initial set of dotfiles added

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,41 @@
+
+if ($host.Name -eq 'ConsoleHost') {
+    Import-Module PSReadLine
+
+    # PsRead improved command history (g + arrow keys)
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+    # Menu autocompletion
+    Set-PSReadLineKeyHandler -Key Tab -Function Complete # Bash like mode
+    #Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete # zsh mode
+
+    # Command prediction based on history
+    Set-PSReadLineOption -PredictionSource History
+
+    # Programmable shortcuts
+    ## Copy current full path to the clipboard
+    Set-PSReadlineKeyHandler -Key Ctrl+Shift+C `
+        -BriefDescription CopyPathToClipboard `
+        -LongDescription "Copies the current path to the clipboard" `
+        -ScriptBlock { (Resolve-Path -LiteralPath $pwd).ProviderPath.Trim() | clip }
+}
+
+# Import previously installed modules (with Install-Module)
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Prompt
+
+# jump to frequently used directories with z
+Import-Module z
+
+# DISABLED FOR NOW
+## use the coreutils ls instead of Get-ChildItem
+# function ls_alias { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
+# Set-Alias -Name ls -Value ls_alias -Option Private
+
+function fn_config { &"C:\Program Files\Git\cmd\git.exe" --git-dir=$home\.cfg\ --work-tree=$home $args }
+Set-Alias -Name config -Value fn_config -Option Private
+
